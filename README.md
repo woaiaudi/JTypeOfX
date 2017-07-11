307c5e8e78327cc8de2cf8cba50a7c3aa10004d0
woaiaudi <zyuemoon@163.com>
2017-07-11 17:40:48 +0800

    泛型基础
    泛型基础

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # JTypeOfX
-Java泛型梳理
+### Java泛型基础
+- T一定是派生于Object类的
+#### 定义变量
+```
+private T x ;
+```
+
+#### 作为返回值
+```
+public T getX(){
+    return x ;
+}
+```
+
+#### 作为参数
+```
+public void setX(T x){
+    this.x = x ;
+}
+```
+
---
+++ src/com/f1code/PointTest.java
@@ -0,0 +1,21 @@
+package com.f1code;
+
+/**
+ * Created by Administrator on 2017/7/11.
+ */
+public class PointTest {
+    public static void main(String[] args) {
+        Point<Integer> pint = new Point<Integer>();
+        pint.setX(12);
+        pint.setY(60);
+
+        System.out.println(pint.getX() + "==" + pint.getY());
+
+        Point<Float> pfloat = new Point<Float>();
+        pfloat.setX(12.5f);
+        pfloat.setY(24.5f);
+
+        System.out.println(pfloat.getX() + "==" + pfloat.getY());
+
+    }
+}
